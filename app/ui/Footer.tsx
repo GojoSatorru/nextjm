33c727ae135fd2691b2609441a5cbbdb1e395d4a
LyhourMam <144458914+LyhuaMam@users.noreply.github.com>
2023-11-09 10:50:13 +0700

    change footer

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <footer className={ `${poppins.className} flex justify-between m-4 text-gray-500`}>
       <p>@Copyright All right reserved.  <br /> 09/Nov/2023</p>
-      <p>By Mam Lyhua</p>
+      <p>By Mam Lyhour</p>
     </footer>
   )
 }
